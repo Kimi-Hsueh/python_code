da376e3a206533e80db3201a2ee5b6a8097a75f7
Kimi-Hsueh <kimi80505@gmail.com>
2023-09-14 23:40:52 +0800

    資料類型轉換完成

--- 20230911/數值計算與資料輸入.py
+++ 20230911/數值計算與資料輸入.py
@@ -109,3 +109,102 @@ print(0b10)
 print(0o10)
 #在16進制輸入10
 print(0x10)
+
+#類型轉換
+'''
+將內容轉換為不同的資料型態,共4種類型
+1 轉換為整數 
+2 轉換為浮點數
+3 轉換為布林值
+4 轉換為字串
+'''
+#1 轉換為整數
+'''
+利用 int ( ) 可分別將浮點數、布林值、字串轉換為整數
+若符點數非整數，轉換成整數時原則上向 0 取整數
+將布林值轉換為整數時，True 會得到 1 而 False 會得到 0
+字串內的字元必須是整數才能進行轉換
+'''
+#浮點數轉換為整數
+print(int(4.99)) 
+#布林值轉為整數
+print(int(True))
+print(int(False))
+#正負值轉換為整數
+print(int('99'))
+print(int('-23'))
+print(int('+12'))
+print(int(-23))
+'''
+#文字肉容不可以轉換為整數
+print(int('99 bottles of beer on the wall'))
+#出現錯誤 ValueError: invalid literal for int() with base 10: '99 bottles of beer on the wall'
+print(type('99 bottles of beer on the wall'))
+#結果為<class 'str'>
+#因為變數為文字型態
+print(int('98.6')) 
+#出現錯誤 ValueError: invalid literal for int() with base 10: '98.6'
+print(type('98.6'))
+#結果為<class 'str'>
+#因為變數為文字型態,小數點被判讀為英文符號的句點
+'''
+#運算結果轉換為整數
+print(4+7.0)
+print(int(4+7.0))
+print(True+2)
+print(int(True+2))
+print(False+5.0)
+print(int(false+5.0))
+#int的範圍
+go =10**1000
+print(go)
+print(go*go)
+
+#2 轉換為浮點數
+'''
+利用 float ( ) 可分別將整數、布林值、字串轉換為浮點數
+將布林值轉換為浮點數時，True 會得到 1.0 而 False 會得到 0.0
+字串內的字元必須是數值才能進行轉換
+'''
+#布林轉換為浮點數(結果會自動在小數點後面補1個0)
+print(float(True))
+print(int(False))
+#整數轉換為浮點數(結果會自動在小數點後面補1個0)
+print(float(98))
+print(float('99'))
+#將帶有小數點的文字類型數字轉換為浮點數
+print(float('98.6')) 
+print(float('-1.5'))
+print(float('1.0e4'))
+#與轉換整數不同的是，此處的文字句點被視為浮點
+
+#轉換為布林值
+'''
+利用 bool ( ) 可分別將整數、浮點數、字串轉換為布林值
+原始資料型態為數值時，僅傳入 0 或 0.0 時會回傳 False，其餘皆為 True
+原始資料型態為字串時，僅傳入空字串會回傳 False ，其餘皆為 True
+'''
+print(bool(4.79))
+print(bool(13245))
+print(bool(0))
+print(bool(0.0))
+print(bool('False'))
+print(bool(-5))
+print(bool(''))
+
+#將資料型態轉換為字串
+'''
+用法 : str ( 欲轉換之內容 )
+利用 str ( ) 可分別將整數、浮點數、布林值轉換為字串
+'''
+print(type(str(377)))
+# <class 'str'>
+print(type(str(-50)))
+# <class 'str'>
+print(type(str(5.0)))
+# <class 'str'>
+print(type(str(True)))
+# <class 'str'>
+print(type(str(False)))
+# <class 'str'>
+
