747ad8c80cc88fbed41763cfd4e80fe41c31e89b
Kimi-Hsueh <kimi80505@gmail.com>
2023-09-28 17:18:45 +0800

    上傳0928作業

---
+++ 20230928-object/private_attribute.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self,name:str,wight:int,height:int):
+        self.__name = name #將name改為private_attribute(read only狀態)
+        self.__weight = wight
+        self.height = height
+
+    @property #property ->將設定值設定為read only，不可以更改
+    def weight(self) ->int:
+        return self.__weight
+    @property
+    def getBMI(self) ->float:
+        return self.bmi() #此行去執行method的bmi運算並回傳
+    #method
+    def bmi(self) ->float:
+        return f'BMI={round(self.__weight/(self.height/100)**2,ndigits=2)}'
+
+    def __str__(self) -> str:
+        return f"name={self.__name}\nweight={self.__weight}\nheight={self.height}"
+    
+
+if __name__ == '__main__':
+    p1 = Person("kimi",74,172)
+    p1.__name="vivian" #此行未動態更改是因為name這個attribute被設定為read only了
+    #p1.weight=99  #此行會出現錯誤狀態"AttributeError: can't set attribute"，代表在這個class裡面，是不可以被更改的
+    print(p1)
+    print(p1.getBMI) #getBMI是單項的指令，利用property的getBMI變數去method進行計算
---
+++ 20230928-object/物件導向.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self,name:str,weight:int,height:int):
+        self.name = name
+        self.weight = weight
+        self.height = height
+
+    #method
+    def bmi(self) ->float:
+        return round(self.weight/(self.height/100)**2,ndigits=2)
+
+    def __str__(self) -> str:
+        return f"name={self.name}\nweight={self.weight}\nheight={self.height}"
+    
+
+if __name__ == '__main__':
+    p1 = Person("robert",78,183)
+    print(p1)
+    print(p1.bmi())
