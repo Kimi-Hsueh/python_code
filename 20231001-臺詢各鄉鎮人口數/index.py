f844e69ea05bf71da962b3e0e4bd58c0f8f4387c
Kimi-Hsueh <kimi80505@gmail.com>
2023-10-11 16:30:27 +0800

    modify

--- 20231001-臺詢各鄉鎮人口數/index.py
+++ 20231001-臺詢各鄉鎮人口數/index.py
@@ -2,7 +2,9 @@ import dataSource #匯入已建立download method的py檔案
 
 def main():
     name=dataSource.cityNames()
-    print(name)
+    city = dataSource.info(name='新北市中和區')
+
+    print(city)
 
 if __name__ == '__main__':
     main()
