e961d8348f8cecab2ddba6b580fc929c2841102d
Kimi-Hsueh <kimi80505@gmail.com>
2023-10-12 14:36:29 +0800

    增加上、下部欄位及選單的設定

--- 20231012-視窗化/index1.py
+++ 20231012-視窗化/index1.py
@@ -5,9 +5,21 @@ from tkinter import ttk
 class Winodwset(Tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        self.title('這是py-tkinter視窗') #使用tk模組命名視窗名稱
-        label=Tk.Label(self,text="我是kimi，不是mikey",font=('Helvetica', '30')) #設定視窗內文字內容及文字格式
+        self.title('111年度人口統計') #使用tk模組命名視窗名稱
+        self.configure(background='red') #設定視窗內的背景色
+
+        topFrame=Tk.Frame(self,background='#86C166') #設定視窗上部欄位底色
+        label=Tk.Label(topFrame,text="111年度人口統計",font=('Helvetica', '30')) #設定視窗內文字內容及文字格式
         label.pack(padx=200,pady=100) #設定視窗大小，x是寬度，y是高度
+        topFrame.pack()
+
+        bottomFrame=Tk.Frame(self,background='#8B81C3') #設定視窗下部欄位
+        choies = dataSource.cityNames() #去dataSource呼叫cityNames()method
+        choiesvar=Tk.StringVar(value=choies) #設定選單變數“choiesvar”
+        listbox=Tk.Listbox(bottomFrame,listvariable=choiesvar,width=12) #設定下拉選單變數“listbox”在視窗的下部欄位，選單內容以“choiesvar”為內容，選單寬度為12px
+        listbox.pack()
+        bottomFrame.pack(expand=True,fill='x')
+        
         
 
 def main():
