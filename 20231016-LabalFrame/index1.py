f437b021235f3d262dfd65f858ee8563811d1ff1
Kimi-Hsueh <kimi80505@gmail.com>
2023-10-16 16:36:10 +0800

    modify

--- 20231016-LabalFrame/index1.py
+++ 20231016-LabalFrame/index1.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+from tkinter.simpledialog import Dialog
 
 
 class Window(tk.Tk):
@@ -11,6 +12,42 @@ class Window(tk.Tk):
         #self.geometry('300x200')
         #self.configure(background='#CA7853')
 
+class GetPassword(Dialog):
+
+    def body(self, master):
+        self.title("Enter New Password")
+
+        tk.Label(master, text='Old Password:').grid(row=0, sticky=tk.W)
+        tk.Label(master, text='New Password:').grid(row=1, sticky=tk.W)
+        tk.Label(master, text='Enter New Password Again:').grid(row=2, sticky=tk.W)
+
+        self.oldpw = tk.Entry(master, width=16, show='*')
+        self.newpw1 = tk.Entry(master, width=16, show='*')
+        self.newpw2 = tk.Entry(master, width=16, show='*')
+
+        self.oldpw.grid(row=0, column=1, sticky=tk.W)
+        self.newpw1.grid(row=1, column=1, sticky=tk.W)
+        self.newpw2.grid(row=2, column=1, sticky=tk.W)
+        return self.oldpw
+    
+    def buttonbox(self):
+        '''add standard button box.
+
+        override if you do not want the standard buttons
+        '''
+
+        box = tk.Frame(self)
+
+        w = tk.Button(box, text="確認", width=10, command=self.ok, default=tk.ACTIVE)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+        w = tk.Button(box, text="取消", width=10, command=self.cancel)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack()
+        
 
 
 class MyFrame(tk.LabelFrame):
@@ -22,7 +59,7 @@ class MyFrame(tk.LabelFrame):
         self.tree.heading('#1',text='第一欄')
         self.tree.heading('#2',text='第二欄')
         self.tree.heading('#3',text='第三欄')
-        self.tree.pack()
+        
 
         contacts = []
         for i in range(1,100):
@@ -30,6 +67,16 @@ class MyFrame(tk.LabelFrame):
         
         for contact in contacts:
             self.tree.insert('',tk.END,values=contact)
+
+        self.tree.pack()
+
+        self.tree.bind('<<TreeviewSelect>>',self.item_selected)
+
+    def item_selected(self,event):
+        item_id=self.tree.selection()[0]
+        item_dict=self.tree.item(item_id)
+        print(item_dict['values'])
+        dialog=GetPassword(self)
 
     def choised(self):
         print(self.aligement.get())
