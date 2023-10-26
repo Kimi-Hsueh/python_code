99d9e0ac9462fa035f6b85a8bbdcd70bd0daf941
Kimi-Hsueh <kimi80505@gmail.com>
2023-10-26 16:48:29 +0800

    modify

--- 20231026-youbike/index.py
+++ 20231026-youbike/index.py
@@ -13,25 +13,13 @@ class Window(tk.Tk):
         except Exception:
             messagebox.showerror('下載錯誤','網路狀態不正常\n將關閉視窗\n請稍候再重試')
             self.destroy()
-'''
-def on_closing(w:Window):
-    print('結束視窗')
-    t.cancel()
-    w.destroy()
 
 t=None
-def update_data()->None: #設定更新資料的時間
-    print('做事')
-    global t #這邊是把全域變數t引進到method“update_data”內
-    t=Timer(20,update_data) #每20秒執行一次
-    t.start()#程式開始運作時即啟動
-'''
-t=None
 def main():
-    def on_closing(w:Window):
+    def on_closing():
         print('結束視窗')
         t.cancel()
-        w.destroy()
+        window.destroy()
     
     def update_data()->None: #設定更新資料的時間
         print('做事')
@@ -43,7 +31,7 @@ def main():
     window.geometry('600x300') #建立視窗大小
     window.resizable(width=False,height=False) #限定視窗大小無法變化
     update_data()
-    window.protocol('WM_DELETE_WINDOW', lambda:on_closing(window))
+    window.protocol('WM_DELETE_WINDOW',on_closing)
     window.mainloop()
 
 
