edbbf0c2db2c39aff1f84c492288b3c6ca8d3884
Kimi-Hsueh <kimi80505@gmail.com>
2023-11-01 16:00:56 +0800

    modify

--- 20231026-youbike/index3.py
+++ 20231026-youbike/index3.py
@@ -1,49 +1,50 @@
 import tkinter as tk
 from tkinter import ttk
-#從youbikeTreeView.py檔讀取treeview的內容
 from youbikeTreeView import YoubikeTreeView
 from tkinter import messagebox
 from threading import Timer
-import datasource1
+import datasource
 
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        #-----更新資料庫-----#
+        #---------更新資料庫資料-----------------#
         try:
-            datasource1.update_sqlite_data()
+            datasource.updata_sqlite_data()
         except Exception:
             messagebox.showerror("錯誤",'網路不正常\n將關閉應用程式\n請稍後再試')
             self.destroy()
         
-        #-----建立界面-----#
-        #設定視窗的第一個區塊
-        #設定框架樣式
-        topFrame=tk.Frame(self,relief=tk.RAISED,borderwidth=1)
-        #設定內文樣式
-        tk.Label(topFrame,text='台北市youbike及時資料',font=('arial,20'),bg='#86C166',fg='black',padx=20,pady=20).pack(padx=10,pady=10)
-        #將框架及內文設定打包
+
+        #---------建立介面------------------------
+        #print(datasource.lastest_datetime_data())
+        topFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1)
+        tk.Label(topFrame,text="台北市youbike及時資料",font=("arial", 20), bg="#333333", fg='#ffffff',padx=10,pady=10).pack(padx=20,pady=20)
         topFrame.pack(pady=30)
 
-        #設定視窗的第二個區塊
-        bottomFrame=tk.Frame(self)
-        #-----建立treeview表格-----#        
-        self.youbikeTreeView=YoubikeTreeView(bottomFrame,show='headings',columns=('sna','mday','sarea','ar','tot','sbi','bemp')) #定義資料欄位
+        bottomFrame = tk.Frame(self)
+        #---------------建立treeView---------------
+        self.youbikeTreeView = YoubikeTreeView(bottomFrame,show="headings",columns=('sna','mday','sarea','ar','tot','sbi','bemp'))
         self.youbikeTreeView.pack(side='left')
         vsb = ttk.Scrollbar(bottomFrame, orient="vertical", command=self.youbikeTreeView.yview)
         vsb.pack(side='left')
         self.youbikeTreeView.configure(yscrollcommand=vsb.set)
         bottomFrame.pack(pady=30)
+        
 
-   
+        
+
+
 def main():    
     def update_data(w:Window)->None:
-        datasource1.update_sqlite_data()
-        #-----更新treeView資料-----#
-        lastest_data = datasource1.lastest_datetime_data()
+        datasource.updata_sqlite_data()
+        #-----------更新treeView資料---------------
+        lastest_data = datasource.lastest_datetime_data()
         w.youbikeTreeView.update_content(lastest_data)
-        window.after(60*1000,update_data,w)
+
+        window.after(3*60*1000,update_data,w) #每隔3分鐘
           
+
     window = Window()
     window.title('台北市youbike2.0')
     #window.geometry('600x300')
