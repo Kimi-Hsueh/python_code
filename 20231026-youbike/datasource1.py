de0ba60d149fb8fde45dfc1658d28313787d032b
Kimi-Hsueh <kimi80505@gmail.com>
2023-11-02 12:04:46 +0800

    modify

--- 20231026-youbike/datasource1.py
+++ 20231026-youbike/datasource1.py
@@ -1,20 +1,20 @@
 import requests
 import sqlite3
 
-__all__ = ['update_sqlite_data'] #公開給其他人看的method
+__all__ = ['updata_sqlite_data']
 
-def __download_youbike_data() ->list[dict]:
+def __download_youbike_data()->list[dict]:
     '''
-    下載youbike資料
-    2.0資料來源：https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json
+    下載台北市youbike資料2.0
+    https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json
     '''
-    url ='https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
-    response=requests.get(url)
+    youbike_url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+    response = requests.get(youbike_url)
     response.raise_for_status()
-    print('下載成功')
+    print("下載成功")
     return response.json()
 
-def __create_table(conn:sqlite3.Connection):
+def __create_table(conn:sqlite3.Connection):    
     cursor = conn.cursor()
     cursor.execute(
         '''
@@ -34,63 +34,55 @@ def __create_table(conn:sqlite3.Connection):
     )
     conn.commit()
     cursor.close()
-    #print("create_table成功")
-    
+    print("create_table成功")
 
-def __insert_data(conn:sqlite3.Connection,values:list[any]) ->None :
-    cursor=conn.cursor()
+def __insert_data(conn:sqlite3.Connection,values:list[any])->None:
+    cursor = conn.cursor()
     sql = '''
-    REPLACE INTO 台北市youbike(站點名稱,行政區,更新時間,地址,總車輛數,可借數量,可還車)
+    REPLACE INTO 台北市youbike(站點名稱,行政區,更新時間,地址,總車輛數,可借,可還)
         VALUES(?,?,?,?,?,?,?)
     '''
-    cursor.execute(sql,values)
+    cursor.execute(sql,values)    
     conn.commit()
     cursor.close()
 
-
-
-def update_sqlite_data(): #將json檔匯入到資料庫
+def updata_sqlite_data()->None:
     '''
-    下載/更新資料庫
+    下載,並更新資料庫
     '''
-    data= __download_youbike_data() #呼叫method
-    conn = sqlite3.connect("youbike.db")
+    data = __download_youbike_data()
+    conn = sqlite3.connect("youbike.db")    
     __create_table(conn)
     for item in data:
         __insert_data(conn,[item['sna'],item['sarea'],item['mday'],item['ar'],item['tot'],item['sbi'],item['bemp']])
     conn.close()
 
-def lastest_datetime_data():
-     '''
-     從資料庫撈取資料
-     '''
-     conn=sqlite3.connect('youbike.db') #指定要連線的資料庫
-     cursor=conn.cursor()
-     #指定選取資料的語法
-     sql='''
-    select 站點名稱,MAX(更新時間) AS 更新時間,行政區,地址,總車輛數,可借數量,可還車
+def lastest_datetime_data()->list[tuple]:
+    conn = sqlite3.connect("youbike.db")
+    cursor = conn.cursor()
+    sql = '''
+    SELECT 站點名稱,MAX(更新時間) AS 更新時間,行政區,地址,總車輛數,可借,可還
     FROM 台北市youbike
     GROUP BY 站點名稱
     '''
-     cursor.execute(sql)
-     rows=cursor.fetchall()
-     cursor.close()
-     conn.close()
-
-     return rows
-
-def search_sitename(word:str) ->list[tuple]:
-    conn=sqlite3.connect('youbike.db')
-    cursor=conn.cursor()
-    sql='''
-    SELECT 站點名稱,max(更新時間) AS 更新時間,行政區,地址,總車輛數,可借,可還
-    FROM 台北市youbike
-    GROUP BY 站點名稱
-    HAVING 站點名稱 like ?
-'''
-    cursor.execute(sql,f'%{word}%')
-    rows=cursor.fetchall()
+    cursor.execute(sql)
+    rows = cursor.fetchall()
     cursor.close()
+    conn.close()
     
+    return rows
 
-
+def search_sitename(word:str) -> list[tuple]:
+    conn = sqlite3.connect("youbike.db")
+    cursor = conn.cursor()
+    sql = '''
+        SELECT 站點名稱,MAX(更新時間) AS 更新時間,行政區,地址,總車輛數,可借,可還
+        FROM 台北市youbike
+        GROUP BY 站點名稱
+        HAVING 站點名稱 like ?
+        '''
+    cursor.execute(sql,[f'%{word}%'])
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return rows
--- 20231026-youbike/youbikeTreeView.py
+++ 20231026-youbike/youbikeTreeView.py
@@ -3,32 +3,44 @@ from tkinter import ttk
 class YoubikeTreeView(ttk.Treeview):
     def __init__(self,parent,**kwargs):
         super().__init__(parent,**kwargs)
-        #-----設定欄位名稱-----#
+        #------設定欄位名稱---------------
         self.heading('sna',text='站點名稱')
         self.heading('mday',text='更新時間')
         self.heading('sarea',text='行政區')
         self.heading('ar',text='地址')
         self.heading('tot',text='總車輛數')
-        self.heading('sbi',text='可借數量')
-        self.heading('bemp',text='可還車')
+        self.heading('sbi',text='可借')
+        self.heading('bemp',text='可還')
 
-        #-----設定欄位寬度-----#
-        #語法：self.column('欄位名',width=整數數字)
+        #----------設定欄位寬度------------
         self.column('sna',width=200)
         self.column('mday',width=150)
         self.column('sarea',width=50)
         self.column('ar',width=300)
-        self.column('tot',width=80)
-        self.column('sbi',width=80)
-        self.column('bemp',width=80)
+        self.column('tot',width=50)
+        self.column('sbi',width=50)
+        self.column('bemp',width=50)
+
+        #-----bind button1-----#
+        self.bind('<ButtonRelease-1>', self.selectedItem)
+
 
     def update_content(self,site_datas):
         '''
-        更新站點資料狀況
+        更新內容
         '''
-        #-----清除所有內容-----#
-        for item in self.get_children():
-            self.delete(item)
+        #清除所有內容
+        for i in self.get_children():
+            self.delete(i)
+        
+        for index,site in enumerate(site_datas):
+            self.insert('','end',text=f"abc{index}",values=site)
 
-        for site in site_datas:
-            self.insert('','end',values=site)
+    def selectedItem(self,event):
+        selectedItem = self.focus()
+        print(selectedItem)
+        print(self.item(selectedItem))
+
+
+
+        
