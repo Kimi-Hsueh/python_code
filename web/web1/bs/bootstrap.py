7ac1cb35a71ffc9fa8f3606c989da5eeb4a24abb
Kimi-Hsueh <kimi80505@gmail.com>
2023-11-22 16:47:01 +0800

    更新20231122上課內容

--- web/web1/bs/bootstrap.py
+++ web/web1/bs/bootstrap.py
@@ -3,4 +3,12 @@ bp = Blueprint('bs', __name__, url_prefix='/bs')
 
 @bp.route("/")
 def index():
-    return render_template('bs/index.html')
+    return render_template("bs/index.html")
+
+@bp.route("/product")
+def product():
+    return render_template('bs/product.html')
+
+@bp.route("/profile")
+def profile():
+    return render_template('bs/profile.html')
