b650271a82c40b2bae154b98d5c93533e424170f
Kimi-Hsueh <kimi80505@gmail.com>
2023-12-04 15:34:26 +0800

    modify

--- flask_web/main.py
+++ flask_web/main.py
@@ -1,6 +1,13 @@
 from flask import Flask,render_template,url_for
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from dash_file import app
 
 app = Flask(__name__)
+application = DispatcherMiddleware(
+    app,
+    {"/app1": app1.server, "/app2": app2.server},
+)
+
 
 @app.route("/")
 def index():
