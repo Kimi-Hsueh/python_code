6864995f1cf4dd678b35e06ba5ec7670a2088bac
Kimi-Hsueh <kimi80505@gmail.com>
2023-12-04 16:43:35 +0800

    完成dash與flask結合檔案

--- flask_web/main.py
+++ flask_web/main.py
@@ -1,14 +1,17 @@
 from flask import Flask,render_template,url_for
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from dash_file import app
+from dash_file.dash_app1 import dash1
+from werkzeug.serving import run_simple
 
 app = Flask(__name__)
 application = DispatcherMiddleware(
     app,
-    {"/app1": app1.server, "/app2": app2.server},
-)
-
-
+    {"/app1": dash1.server},
+) #中繼app，我要產生一個中繼的app
+#
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    run_simple("localhost", 8080, application,use_debugger=True,use_reloader=True)
