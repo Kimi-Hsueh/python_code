39543b03df536c35d14e2b1476a938c7b69d1314
Kimi-Hsueh <kimi80505@gmail.com>
2023-12-06 09:39:24 +0800

    上傳至render

--- flask_web/main.py
+++ flask_web/main.py
@@ -1,14 +1,15 @@
 from flask import Flask,render_template,url_for
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from dash_file.dash_app1 import dash1
 from werkzeug.serving import run_simple
+from dash_file.dash_app1 import dash1
 
 app = Flask(__name__)
+
 application = DispatcherMiddleware(
     app,
-    {"/app1": dash1.server},
-) #中繼app，我要產生一個中繼的app
-#
+    {"/dash/app1": dash1.server}
+)
+
 @app.route("/")
 def index():
     return render_template("index.html")
