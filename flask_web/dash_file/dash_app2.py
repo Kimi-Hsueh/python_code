819b214bb61165cc1914cd780846f750fee86543
Kimi-Hsueh <kimi80505@gmail.com>
2023-12-06 15:47:36 +0800

    新增youbike function

--- flask_web/dash_file/dash_app2.py
+++ flask_web/dash_file/dash_app2.py
@@ -1,15 +1,49 @@
-from dash import Dash, html
+from dash import Dash, html,dash_table
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
+from collections import OrderedDict
 
 #-----指定網頁路徑的名稱及套用dash_bootstrap_components設定-----#
 dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-dash2.layout =html.Div(
-    [html.H1('全台灣最混的網站上線嚕'),
-     html.P('24小時無聊男子不在線'),
-     html.P('這是用dash-bootstrap做的')],
-     style={'backgroundColor':'aqua'},
-     className="container-lg"
+#-----頁籤的命名-----#
+dash2.title='DASH的示範網頁'
+data = OrderedDict(
+    [
+        ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
+        ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
+        ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
+        ("Humidity", [10, 20, 30, 40, 50, 60]),
+        ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
+    ])
+
+df = pd.DataFrame(
+    OrderedDict([(name, col_data * 10) for (name, col_data) in data.items()])
 )
+
+dash2.layout = html.Div(
+    [
+        dbc.Container([
+            html.Div([
+                html.Div([
+                    html.H1("台北市youbike及時資料")
+                ],className="col text-center")
+            ],
+            className="row",
+            style={"paddingTop":'2rem'}),
+            html.Div([
+                html.Div([
+                    dash_table.DataTable(
+                        data=df.to_dict('records'),
+                        columns=[{'id':column,'name':column} for column in df.columns],
+                        page_size=20
+                    ),
+                ],className="col text-center")
+            ],
+            className="row",
+            style={"paddingTop":'2rem'}),
+        ])
+    ],
+    className="container-lg"
+    )
