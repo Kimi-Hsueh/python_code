843862e2927cc3f2d367f6f97055f5c4ec8cfbd3
Kimi-Hsueh <kimi80505@gmail.com>
2023-12-06 13:36:57 +0800

    開始設置自訂的html與css

--- flask_web/dash_file/dash_app2.py
+++ flask_web/dash_file/dash_app2.py
@@ -1,11 +1,15 @@
 from dash import Dash, html
 import plotly.express as px
 import pandas as pd
+import dash_bootstrap_components as dbc
 
-dash2 = Dash(requests_pathname_prefix="/dash/app2/")
+#-----指定網頁路徑的名稱及套用dash_bootstrap_components設定-----#
+dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-dash2.layout =html.Div([
-    html.H1('DASH H1'),
-    html.P('台灣最混的網頁上線嚕'),
-    html.P('無聊男子24小時不在線')
-])
+dash2.layout =html.Div(
+    [html.H1('全台灣最混的網站上線嚕'),
+     html.P('24小時無聊男子不在線'),
+     html.P('這是用dash-bootstrap做的')],
+     style={'backgroundColor':'aqua'},
+     className="container-lg"
+)
