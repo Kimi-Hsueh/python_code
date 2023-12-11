803b643b93a7ee5cf6539ec5fbd6d913c0427d9f
Kimi-Hsueh <kimi80505@gmail.com>
2023-12-11 11:55:45 +0800

    新增單選按鈕動作及顯示

--- flask_web/dash_file/dash_app2.py
+++ flask_web/dash_file/dash_app2.py
@@ -1,4 +1,4 @@
-from dash import Dash, html,dash_table
+from dash import Dash, html,dash_table,Input,Output,callback
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
@@ -32,17 +32,49 @@ dash2.layout = html.Div(
             html.Div([
                 html.Div([
                     dash_table.DataTable(
+                        id='main_table',
                         data=lastest_df1.to_dict('records'),
-                        columns=[{'id':column,'name':column} for column in lastest_df.columns],
+                        columns=[{'id':column,'name':column} for column in lastest_df1.columns],
                         page_size=20,
                         style_table={'height': '300px', 'overflowY': 'auto'},
-                        fixed_rows={'headers': True}
+                        fixed_rows={'headers': True},
+                        style_cell_conditional=[
+                                {   'if': {'column_id': 'index'},
+                                 'width': '5%'
+                                },
+                                {   'if': {'column_id': '站點名稱'},
+                                 'width': '25%'},
+                                {   'if': {'column_id': '總數'},
+                                 'width': '5%'},
+                                {   'if': {'column_id': '可借'},
+                                 'width': '5%'},
+                                {   'if': {'column_id': '可還'},
+                                 'width': '5%'},
+                        ],
+                        row_selectable="single", #在表單內增加單選按鈕
+                        selected_rows=[] #預設單選按鈕為“不選擇”
                     ),
                 ],className="col text-center")
             ],
             className="row",
             style={"paddingTop":'2rem'}),
+            html.Div([
+                html.H5("這是第3列",className="col",id='showMessage') #在網頁上新增第三個顯示區塊
+            ],
+            className="row",
+            style={"paddingTop":'2rem'})
+
         ])
     ],
     className="container-lg"
     )
+
+
+# 註冊單選按鈕動作
+@callback(
+      Output('showMessage','children'),
+      Input('main_table','selected_rows')  
+)
+def selectedRow(selected_rows):
+    print(selected_rows)
+    return str(selected_rows)
